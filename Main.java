adda0ab8653b9f93152de6520489e1718e57d84e
Badger <mcomia370@gmail.com>
2025-08-22 21:57:27 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.print("Hello and welcome!");
+
+//        int a = 5;
+//        char b = 10;
+//        long c = 15;
+//       double d = 2.0;
+
+//        String name = "Badger";
+//        System.out.print(name.toUpperCase());
+//        System.out.print(name.toLowerCase());
+    }
+}
