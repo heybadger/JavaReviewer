6ff4f9c08bd210ef8433bda42cacd0397d6e9ab7
Badger <mcomia370@gmail.com>
2025-08-22 22:48:20 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.print("Hello and welcome!");
+//        System.out.print("Hello and welcome!"); // Print Line
 
 //        int a = 5;
 //        char b = 10;
 //        long c = 15;
 //       double d = 2.0;
 
-//        String name = "Badger";
+//        String name = "Badger"; // Object
 //        System.out.print(name.toUpperCase());
-//        System.out.print(name.toLowerCase());
+//        System.out.print(name.toLowerCase()); // Methods
+
+// public static void addExclamationPoint(String s) {
+// }
     }
 }
